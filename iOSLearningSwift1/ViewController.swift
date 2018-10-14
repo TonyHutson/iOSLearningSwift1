9a0bd5be569ea68df853c3be38ab4101d84e7e8a
Tony Hutson <tony.hutson@tonyhutson.com>
2018-10-14 10:22:07 -0400

    Initial Commit

---
+++ iOSLearningSwift1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLearningSwift1
+//
+//  Created by Dwayne Hutson on 10/14/18.
+//  Copyright © 2018 Tony Hutson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
